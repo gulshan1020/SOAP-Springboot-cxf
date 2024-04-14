cfcd1b1737cc572af2f6fdf10817a269240230ff
gulshan1020 <42057597+gulshan1020@users.noreply.github.com>
2024-04-14 12:48:16 +0530

    Class Rename

--- src/main/java/com/bharath/ws/soap/CustomerOrderWsImpl.java
+++ src/main/java/com/bharath/ws/soap/CustomerOrderWsImpl.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-@Service
+
 public class CustomerOrderWsImpl implements CustomerOrdersPortType {
 
     Map<BigInteger, List<Order>> customerOrders = new HashMap<>();
